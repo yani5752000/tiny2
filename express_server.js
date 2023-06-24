5c828ee4aefea412c1ba69f36e6a443eb0205082
Farid Asadpour <84594007+yani5752000@users.noreply.github.com>
2023-06-24 15:23:29 -0400

    displaying the object

--- express_server.js
+++ express_server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const PORT = 8080;
 const app = express();
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 })
 
 app.get("/", (req, res) => {
     res.send("Hi this is tiny2");
+})
+
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
 })
