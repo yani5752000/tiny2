373f9a5a948971a3c92d37dfb9367e8fa16f5e84
Farid Asadpour <84594007+yani5752000@users.noreply.github.com>
2023-06-25 15:53:15 -0400

    url show

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = 8080;
 const app = express();
+app.set("view engin", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,3 +23,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/helloWorld", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
+
+app.get("/urls", (req, res) => {
+    const templateVars = {urls: urlDatabase};
+    res.render("urls_index.ejs", templateVars);
+})
+
+app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    res.render("urls_show.ejs", templateVars);
+});
